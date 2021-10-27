a6b395ead12e86142b4c1a1c641a3056c68e5799
gary <gary.white@lodgit.com>
2021-10-27 13:08:08 +0200

    code working

--- kataSimpMult.js
+++ kataSimpMult.js
@@ -1,5 +1,10 @@
 function simpleMultiplication(number) {
-    // your code........
+    var result = number % 2; // Use modulo to check if Odd/Even
+    if (result == 0) {
+        return number * 8;
+    } else {
+        return number * 9;
+    }
 }
 
 console.log(simpleMultiplication(2)); // 16
